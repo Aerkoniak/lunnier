3cc77d0e19592c31254a9d20683c2cc0eddef5cc
Michal Szczepanski <lunnier@gmail.com>
2020-11-06 23:43:35 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import AOS from 'aos';
+import './css/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+import './css/aos_delay.scss'
+// ..
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+
+import Footer from './components/Desktop/Footer/Footer';
+import Main from './components/Desktop/Main/Main';
+import Header from './components/Desktop/Header/Header';
+import MobileHeader from './components/Mobile/MobileHeader';
+
+AOS.init({ once: true });
 
 function App() {
+
+
+  // useEffect(() => {
+  //   const app = document.getElementById('#App');
+  //   console.log(app)
+  // }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <div className="App" id="App">
+          <div className="desktop">
+            <Header />
+            <Main />
+            <Footer />
+          </div>
+          <div className="mobile">
+            <MobileHeader />
+          </div>
+
+        </div>
+      </Switch >
+    </BrowserRouter >
+
   );
 }
 
