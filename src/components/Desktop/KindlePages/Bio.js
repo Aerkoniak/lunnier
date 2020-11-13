cfda24644cb55ea194184bb24d18451191c41565
Michal Szczepanski <lunnier@gmail.com>
2020-11-13 17:25:27 +0100

    texts replaced

--- src/components/Desktop/KindlePages/Bio.js
+++ src/components/Desktop/KindlePages/Bio.js
@@ -10,12 +10,12 @@ const Bio = () => {
             <p className="return"><Link to="/">wróć do spisu treści</Link></p>
             <p className="pageNumber">1</p>
             <h3>Trochę o mnie</h3>
-            <p className="par">Jestem trzydziestolatkiem, który w końcu zaczął zajmować się tym, co sprawia mu przyjemność. Liceum z profilem biologiczno-chemicznym, później niezwiązane z biologią ani chemią studia, trzy różne kierunki, każdy porzucony. Blisko dziesięć różnych zawodów w przeciągu pięciu lat i wszystko to po to by ostatecznie zdecydować się na emigrację, w poszukiwaniu jeszcze czegoś innego. Typowy millenials.</p>
+            <p className="par">Jestem trzydziestolatkiem, który może pozwolić sobie na komfort przyznania, że wreszcie zaczął zajmować się tym, co naprawdę sprawia mu przyjemność. Ukończyłem liceum w klasie o profilu biologiczno-chemicznym, by później rozpocząć studia kolejno na trzech różnych kierunkach, przy czym żaden nie był związany z biologią lub chemią. Żadnego z nich nie skończyłem. Na swoim koncie mam blisko dziesięć różnych zawodów i to biorąc pod uwagę jedynie ostatnie pięć lat mojego życia. Ostatecznie, w poszukiwaniu jeszcze czegoś innego, zdecydowałem się na emigrację. Ot, pomyślicie pewnie, typowy millennials.</p>
             <div className="float">
                 <img src={triquetra} alt="triquetra" className="floatedImg" />
-                <p className="par">Wyjazd za granicę i życie za nią przez cztery lata nauczyło mnie pokory oraz solidnie przygotowało grunt pod późniejsze zmiany zarówno charakteru jak i poglądowe. Celtycka triquetra w jednym ze swych znaczeń symbolizuje to, iż wszystko co robimy w życiu powraca do nas później trzykrotnie silniej. Dojrzałem do tego poglądu. </p>
+                <p className="par">Niewykluczone. Wiem jednak, że – przeszło czteroletni – pobyt za granicą nauczył mnie pokory i przygotował solidny grunt pod późniejsze zmiany i to nie tylko te dotyczące charakteru, ale również poglądów. Celtycka triquetra jest symbolem, który w jednym z wielu swoich znaczeń, obrazuje zasadę mówiącą, że cokolwiek w życiu zrobimy, powróci do nas z potrójną siłą. Dojrzałem do tego poglądu.</p>
             </div>
-            <p className="par">Obecnie od blisko półtora roku zajmuje się projektowaniem i programowaniem stron, i wszystkiego innego, co można znaleźć w internecie. Naukę i kursy rozpocząłem jeszcze w Niderlandach, na ten moment jednak wróciłem z powrotem do kraju, postanawiając iż zarówno on jak i ja zasługujemy na jeszcze jedną próbę.</p>
+            <p className="par">Obecnie, już od blisko półtora roku, zajmuję się projektowaniem i programowaniem stron oraz wszystkiego, co można znaleźć w internecie. Naukę i kursy rozpocząłem jeszcze w Niderlandach i kontynuuję je nadal, po powrocie do kraju. Uznałem, że zarówno on, jak i zasługujemy na jeszcze jedną próbę.</p>
             <img src={portret} alt="portret własciciela" className="photo" />
             <p className="return"><Link to="/">wróć do spisu treści</Link></p>
 
