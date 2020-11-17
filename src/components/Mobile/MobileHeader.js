1bccbac47259615b309d6c93287b7e344e6b09d4
Michal Szczepanski <lunnier@gmail.com>
2020-11-17 16:57:32 +0100

    background image replaced for better performance

--- src/components/Mobile/MobileHeader.js
+++ src/components/Mobile/MobileHeader.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import '../../css/mobile_css.css'
 
-// import brokenScreen from '../../css/crack.png';
 import TypedComponent from '../Typed/TypedComponent';
 
 const MobileHeader = () => {
@@ -10,10 +9,10 @@ const MobileHeader = () => {
 
     return (
         <>
-            <section className={isScreenBroken ? "header mobile broken" : "header mobile"}
+            <section className="header mobile broken"
                 onClick={() => brokeScreen(true)}    >
 
-                {isScreenBroken ? <TypedComponent id="mobileBreak" text="Ojej, stłukło się..." delay={250} /> : <p>tapnij w ekran</p>}
+                {isScreenBroken ? <TypedComponent id="mobileBreak" text="Ojej, stłukło się..." delay={250} /> : <div className="curtain"><p>tapnij w ekran</p></div>}
                 <div className={isScreenBroken ? "secondHeader shown" : "secondHeader hide"}>
                     {isScreenBroken ? <TypedComponent id="secondStart" delay={3000} text="Zbudujmy to więc od początku..." /> : null}
                     {isScreenBroken ? <TypedComponent id="secondStartString" delay={5500} text="Nazywam się Michał Szczepański i zawodowo zajmuje się projektowaniem oraz programowaniem stron i aplikacji mobilnych." /> : null}
